49b5b6c50e3b268c1364bfa59d7b6645479c65bf
Gnuk <gnuk@mailoo.org>
2013-10-19 17:48:14 +0200

    Category -> Categories

--- src/Univ/Savoie/EdtBundle/Controller/IcsController.php
+++ src/Univ/Savoie/EdtBundle/Controller/IcsController.php
@@ -60,7 +60,7 @@ class IcsController extends Controller
 			$vEvent->add('DTSTART', $start);
 			$vEvent->add('DTEND', $end);
 			$vEvent->add('SUMMARY', $event['summary']);
-			$vEvent->add('CATEGORY', $event['type']);
+			$vEvent->add('CATEGORIES', $event['type']);
 			if(!empty($event['place']))
 			{
 				$vEvent->add('LOCATION', $event['place']);
