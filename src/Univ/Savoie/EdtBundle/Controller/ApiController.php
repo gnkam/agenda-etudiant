04f98db614b1799a758801e1c20a74e012a31a84
Gnuk <gnuk@mailoo.org>
2013-10-24 14:07:45 +0200

    Treeview service

--- src/Univ/Savoie/EdtBundle/Controller/ApiController.php
+++ src/Univ/Savoie/EdtBundle/Controller/ApiController.php
@@ -16,6 +16,12 @@ use Gnkam\Univ\Savoie\Edt\Formalizer;
 */
 class ApiController extends Controller
 {
+	/**
+	* @var integer
+	* @todo Find a way to get projectId
+	*/
+	private $projectId = 2;
+
     /**
      * @Route("/group/{id}.{format}")
      * @Route("/group/{id}")
@@ -23,9 +29,8 @@ class ApiController extends Controller
      */
     public function groupIdAction($id, $format = 'json')
     {
-		# Use ProjectId
-		$projectId = 2;
-    
+		# 6 Hours update
+		$update = 6 * 60 * 60;
 		$id = intval($id);
 		if(empty($id))
 		{
@@ -36,9 +41,46 @@ class ApiController extends Controller
 			);
 		}
 		
+		# Formalize Data
+		$formalizer = $this->edtFormalizer($format, $update);
+		$json = $formalizer->serviceGroup($id);
+		
+		if(isset($json['error']))
+		{
+			return new FormatedResponse($json, 500, $format);
+		}
+		# Show json
+        return new FormatedResponse($json, 200, $format);
+    }
+    
+     /**
+     * @Route("/tree")
+     * @Route("/tree.{format}")
+     * @Route("/tree/{node}.{format}")
+     * @Route("/tree/{node}")
+     * @Method({"GET"})
+     */
+    public function treeNodeAction($node = -1, $format = 'json')
+    {
+		# 7 Days update
+		$update = 7 * 24 * 60 * 60;
+		
+		# Formalize Data
+		$formalizer = $this->edtFormalizer($format, $update);
+		$json = $formalizer->serviceTree($node);
+		
+		if(isset($json['error']))
+		{
+			return new FormatedResponse($json, 500, $format);
+		}
+		# Show json
+        return new FormatedResponse($json, 200, $format);
+    }
+    
+    public function edtFormalizer($format, $update)
+    {
 		# Cache link
 		$cacheLink = __DIR__ . '/../cache';
-		
 		# Create cache dir if not exists
 		if(!is_dir($cacheLink))
 		{
@@ -52,19 +94,8 @@ class ApiController extends Controller
 				return;
 			}
 		}
-		
-		# 6 Hours update
-		$update = 6 * 60 * 60;
-		
 		# Formalize Data
-		$formalizer = new Formalizer($cacheLink, $update, $projectId);
-		$json = $formalizer->serviceGroup($id);
-		
-		if(isset($json['error']))
-		{
-			return new FormatedResponse($json, 500, $format);
-		}
-		# Show json
-        return new FormatedResponse($json, 200, $format);
+		$formalizer = new Formalizer($cacheLink, $update, $this->projectId);
+		return $formalizer;
     }
 }
